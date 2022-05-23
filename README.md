538d903d42551191f790a856dc910b94fa44af17
Ali Samir <alisamir.2020@azhar.edu.eg>
2022-05-23 20:13:11 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# School-Website
+
+## ALI SAMIR
+
+- Responsive Courses Website Using HTML, CSS, and JavaScript.
+- Developed first with the Mobile First methodology, then for desktop.
+- Compatible with all mobile devices and with a beautiful and pleasant user interface.
+
+## [Website Demo](https://alisamirali.github.io/School-Website/)
+![Online Courses Website Mockup](https://user-images.githubusercontent.com/62913154/169881019-e488b063-e17e-4986-a774-a7c70a78c902.png)
---
+++ js/main.js
@@ -0,0 +1,59 @@
+/* Change Header Background When Scrolling */
+window.addEventListener("scroll", () => {
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scroll", window.scrollY > 0);
+});
+
+/* Show Faq Answer */
+const faqs = document.querySelectorAll(".faq");
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("open");
+
+    // Change Icon
+    const icon = document.querySelector(".faq .faq-icon i");
+
+    if (icon.className === "uil uil-plus") {
+      icon.className = "uil uil-angle-up";
+    } else {
+      icon.className = "uil uil-plus";
+    }
+  });
+});
+
+/* Swiper */
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+  breakpoints: {
+    600: {
+      slidesPerView: 2,
+    },
+  },
+});
+
+/* Show / Hide Nav Menu */
+const menu = document.querySelector(".nav-menu");
+const menuButton = document.querySelector("#menu-btn");
+const menuClose = document.querySelector("#close-btn");
+
+menuButton.addEventListener("click", () => {
+  menu.style.display = "flex";
+  menuClose.style.display = "inline-block";
+  menuButton.style.display = "none";
+});
+
+// Close Menu
+const closeNavMenu = () => {
+  menu.style.display = "none";
+  menuClose.style.display = "none";
+  menuButton.style.display = "inline-block";
+};
+
+menuClose.addEventListener("click", closeNavMenu);
